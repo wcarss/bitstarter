c9f7ede6211222a483de800d594425df8989b273
Wyatt Carss <carss.w@gmail.com>
2013-07-11 02:42:46 -0400

    Fix overlapping encoding code.

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var index_file = "index.html"
 
 app.get('/', function(request, response) {
   response.send(
-    fs.readFileSync(index_file, {encoding:'utf8'}).toString()
+    fs.readFileSync(index_file, {encoding:'utf8'})
   )
 });
 
